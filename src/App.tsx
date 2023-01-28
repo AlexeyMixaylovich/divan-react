037b36dfaf535e618ce05f1c7f6c2432a80f8fb6
alekseyzubkov <aleksey.zubkov@sravni.ru>
2023-01-28 23:51:04 +0300

    init:finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button variant="contained" endIcon={<SendIcon />}>
+      Send
+    </Button>
   );
 }
 
